79549567e27f74d333af53b29d6eaed063465c8f
Lau Lugo <lalacosaleve@icloud.com>
2023-10-10 19:02:08 -0600

    add: Store data

---
+++ src/STORE.js
@@ -0,0 +1,79 @@
+const teamMembers = [
+    {
+        photo: "https://images.wikidexcdn.net/mwuploads/wikidex/thumb/0/0b/latest/20160904204605/Snorlax.png/800px-Snorlax.png",
+        name: "Alonzo Perez",
+        github: "https://github.com/",
+        linkedin: "https://www.linkedin.com/",
+    },
+    {
+        photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        name: "Dante Vásquez",
+        github: "https://github.com/",
+        linkedin: "https://www.linkedin.com/",
+    },
+    {
+        photo: "https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/fb/latest/20200411222755/Charmeleon.png/800px-Charmeleon.png",
+        name: "Jimmy Cahuas",
+        github: "https://github.com/",
+        linkedin: "https://www.linkedin.com/",
+    },
+    {
+        photo: "https://www.latercera.com/resizer/qBNTRKQWHm26rezD6oUGqSqBMic=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/KCV3Q7JXTBEMVAJVBDONB6YIFE.png",
+        name: "Jorge Capcha",
+        github: "https://github.com/",
+        linkedin: "https://www.linkedin.com/",
+    },
+    {
+        photo: "https://images.wikidexcdn.net/mwuploads/wikidex/thumb/2/20/latest/20230613020138/Gligar.png/1200px-Gligar.png",
+        name: "Jorge Estrada",
+        github: "https://github.com/",
+        linkedin: "https://www.linkedin.com/",
+    },
+    {
+        photo: "https://images.wikidexcdn.net/mwuploads/wikidex/thumb/a/af/latest/20150110232910/Jigglypuff.png/800px-Jigglypuff.png",
+        name: "Laura Lugo",
+        github: "https://github.com/",
+        linkedin: "https://www.linkedin.com/",
+    },
+];
+
+const datafake = [
+    {
+        img: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1980&q=80",
+        price: 120000,
+        operation: "rent",
+        type: "apartment",
+        address: "Calle 123, Apartment 1A, Bloque 2, Urbanización La Paz",
+        bed: 2,
+        bath: 1,
+        area: 150,
+        pet: true,
+        footer: true,
+    },
+    {
+        img: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1950&q=80",
+        price: 95000,
+        operation: "rent",
+        type: "house",
+        address: "Avenida Principal 456, Residencial Los Pinos",
+        bed: 4,
+        bath: 3,
+        area: 250,
+        pet: false,
+        footer: false,
+    },
+    {
+        img: "https://media.self.com/photos/630635c30b7f36ce816f374a/2:1/w_1280,c_limit/DAB03919-10470989.jpg",
+        price: 180000,
+        operation: "sale",
+        type: "villa",
+        address: "Calle de las Flores 789, Colonia Primavera",
+        bed: 5,
+        bath: 4,
+        area: 350,
+        pet: true,
+        footer: true,
+    },
+];
+
+export { teamMembers, datafake };
