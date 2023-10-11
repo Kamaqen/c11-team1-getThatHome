6c2e523dfefd0e20ec463b1bc3e34c5bd9d5129b
Alyho <alyhoiz@gmail.com>
2023-10-11 15:16:01 -0500

    Buttonsv2

--- Button.jsx
+++ Button.jsx
@@ -1,47 +1,36 @@
-import React from "react";
 import styled from "@emotion/styled";
 import { RiArrowDownSLine, RiUserLine } from "react-icons/ri";
 
-const IconRiUserLine = styled.div`
+export const Icon = styled.div`
   display: flex;
-  width: 24px;
-  height: 24px;
+  font-size: 24px;
   justify-content: center;
   align-items: center;
 `;
 
-const IconRiArrowDownSLine = styled.div`
-  display: flex;
-  width: 24px;
-  height: 24px;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ButtonComponent = styled.button`
-  width: 161px;
-  height: 40px;
-  top: 8px;
-  left: 8px;
-  position: relative;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
+export const Blayout = styled.div`
+  font-family: "Inter", sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 24px; /* 171.429% */
+  letter-spacing: 1.25px;
+  text-transform: uppercase;
   text-align: center;
-  text-decoration: none;
-  vertical-align: middle;
+  display: inline-flex;
+  border-radius: 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
   cursor: pointer;
   user-select: none;
-  border-radius: 1rem;
-  padding: 0
-    ${(props) =>
-      props.size === "def"
-        ? "8px 16px 8px 16px"
-        : props.size === "lg"
-        ? "16px 24px 16px 24px"
-        : props.size === "sm"
-        ? "4px 8px 4px 8px"
-        : "8px"};
+  padding: ${(props) =>
+    props.size === "def"
+      ? "8px 16px 8px 16px"
+      : props.size === "lg"
+      ? "16px 24px 16px 24px"
+      : props.size === "sm"
+      ? "4px 8px 4px 8px"
+      : "8px"};
   height: ${(props) =>
     props.size === "def"
       ? "40px"
@@ -49,48 +38,63 @@ const ButtonComponent = styled.button`
       ? "56px"
       : props.size === "sm"
       ? "32px"
-      : "40px"};
-  font-family: "Inter", sans-serif;
-  font-weight: 500;
-  border: ${(props) =>
-    props.variant === "secondary" ? "1px solid #F48FB1" : "1px #F48FB1"};
-  color: ${(props) =>
-    props.variant === "primary"
+      : "0px"};
+  width: ${(props) =>
+    props.size === "def"
+      ? "161px"
+      : props.size === "lg"
+      ? "177px"
+      : props.size === "sm"
+      ? "145px"
+      : "0px"};
+  color: ${(
+    props // Text inside the button
+  ) =>
+    props.variant === "Primary"
       ? "#FFFFFF"
-      : props.variant === "secondary"
+      : props.variant === "Secundary"
       ? "#616161"
-      : props.variant === "disabled"
-      ? "#8E8E8E"
-      : props.variant === "ghost"
+      : props.variant === "Disabled"
+      ? "#8e8e8e"
+      : props.variant === "Ghost"
       ? "#616161"
       : "#FFFFFF"};
+  border: ${(props) =>
+    props.variant === "Secundary" ? "1px solid #F48FB1" : "0px solid #BF5F82"};
   background-color: ${(props) =>
-    props.variant === "primary"
+    props.variant === "Primary"
       ? "#F48FB1"
-      : props.variant === "secondary"
+      : props.variant === "Secundary"
       ? "#FFFFFF"
-      : props.variant === "disabled"
+      : props.variant === "Disabled"
       ? "#61616126"
-      : props.variant === "ghost"
+      : props.variant === "Ghost"
       ? "#FFFFFF"
       : "#F48FB1"};
   &:hover {
     background-color: ${(props) =>
-      props.variant === "primary"
+      props.variant === "Primary"
         ? "#BF5F82"
-        : props.variant === "secondary"
-        ? "#F48FB1"
-        : props.variant === "disabled"
+        : props.variant === "Secundary"
+        ? "#f48fb126"
+        : props.variant === "Disabled"
         ? "#61616126"
-        : props.variant === "ghost"
-        ? "#F48FB1"
+        : props.variant === "Ghost"
+        ? "#F48FB126"
         : "#000000"};
   }
 `;
-
-const Button = ({ type, variant, className, id, onClick, size, children }) => {
+export const Button = ({
+  type,
+  variant,
+  className,
+  id,
+  onClick,
+  size,
+  children,
+}) => {
   return (
-    <ButtonComponent
+    <Blayout
       type={type ? type : "button"}
       variant={variant}
       className={className ? `btn-component ${className}` : "btn-component"}
@@ -98,14 +102,14 @@ const Button = ({ type, variant, className, id, onClick, size, children }) => {
       onClick={onClick}
       size={size}
     >
-      <IconRiUserLine>
+      <Icon>
         <RiUserLine />
-      </IconRiUserLine>
+      </Icon>
       {children}
-      <IconRiArrowDownSLine>
+      <Icon>
         <RiArrowDownSLine />
-      </IconRiArrowDownSLine>
-    </ButtonComponent>
+      </Icon>
+    </Blayout>
   );
 };
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Button from "../Button";
+import Button, { Blayout, Icon, IconBell } from "../Button";
+import { RiNotification2Fill, RiNotification2Line } from "react-icons/ri";
 
 const App = () => {
   return (
@@ -19,46 +20,19 @@ const App = () => {
           flexDirection: "column",
         }}
       >
-        <Button size="def" variant="primary">
+        <Button size="def" variant="Primary">
+          {" "}
+          <span>BUTTON</span>
+        </Button>
+        <Button size="def" variant="Secundary">
           {" "}
           BUTTON
         </Button>
-        <Button size="def" variant="secondary">
+        <Button size="def" variant="Disabled">
           {" "}
           BUTTON
         </Button>
-        <Button size="def" variant="disabled">
-          {" "}
-          BUTTON
-        </Button>
-        <Button size="def" variant="ghost">
-          {" "}
-          BUTTON
-        </Button>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          gap: "5px",
-          flexDirection: "column",
-        }}
-      >
-        <Button size="lg" variant="primary">
-          {" "}
-          BUTTON
-        </Button>
-        <Button size="lg" variant="secondary">
-          {" "}
-          BUTTON
-        </Button>
-        <Button size="lg" variant="disabled">
-          {" "}
-          BUTTON
-        </Button>
-        <Button size="lg" variant="ghost">
+        <Button size="def" variant="Ghost">
           {" "}
           BUTTON
         </Button>
@@ -73,25 +47,63 @@ const App = () => {
           flexDirection: "column",
         }}
       >
-        <Button size="sm" variant="primary">
+        <Button size="lg" variant="Primary">
           {" "}
           BUTTON
         </Button>
-        <Button size="sm" variant="secondary">
+        <Button size="lg" variant="Secundary">
           {" "}
           BUTTON
         </Button>
-        <Button size="sm" variant="disabled">
+        <Button size="lg" variant="Disabled">
           {" "}
           BUTTON
         </Button>
-        <Button size="sm" variant="ghost">
+        <Button size="lg" variant="Ghost">
           {" "}
           BUTTON
         </Button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: "5px",
+          flexDirection: "column",
+        }}
+      >
+        <Button size="sm" variant="Primary">
+          {" "}
+          BUTTON
+        </Button>
+        <Button size="sm" variant="Secundary">
+          {" "}
+          BUTTON
+        </Button>
+        <Button size="sm" variant="Disabled">
+          {" "}
+          BUTTON
+        </Button>
+        <Button size="sm" variant="Ghost">
+          {" "}
+          BUTTON
+        </Button>
+      </div>
+      <div>
+        <IconBell>
+          <RiNotification2Line />
+        </IconBell>
+
+        <IconBell>
+          <RiNotification2Fill />
+        </IconBell>
       </div>
     </React.Fragment>
   );
 };
+
+//onClick={() => handleToggle(id)}
 
 export default App;
