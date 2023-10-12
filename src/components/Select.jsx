298f166743cb022cb0b6108c13e9b0eea7a72312
Alonso Perez <edalopp@hotmail.com>
2023-10-12 11:57:35 -0500

    ADD: select  icons

---
+++ src/components/Select.jsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import { RiCheckFill} from "react-icons/ri";
+import { RiCheckboxBlankLine} from "react-icons/ri";
+import { GrRadialSelected} from "react-icons/gr";
+import { GrRadial} from "react-icons/gr";
+import { IoMdClose} from "react-icons/io";
+import { RiArrowDownSLine} from "react-icons/ri";
+
+
+const checkfillIcon = <RiCheckFill />;
+const checkunfillIcon = <RiCheckboxBlankLine />;
+const selectedtrueIcon = < GrRadialSelected />;
+const selectedfalseIcon = <GrRadial />;
+const closeIcon = <IoMdClose />;
+
