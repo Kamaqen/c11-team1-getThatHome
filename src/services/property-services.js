9016d1c827c099ea07f7f830aee8cc712aedb48f
Dante Vasquez <jdantevc@gmail.com>
2023-10-16 13:56:40 -0500

    FIX: urls not saving

--- src/services/property-services.js
+++ src/services/property-services.js
@@ -3,3 +3,9 @@ import apiFetch from "./api-fetch";
 export function getProperties() {
     return apiFetch("/properties");
 }
+export async function createProperty(data = { adress, area, bathrooms, bedrooms, description, is_active, latitude, longitude, maintenance_price, operation_type, pet_friendly, property_price, property_type, rent_value, urls, user_id }) {
+    return await apiFetch(`/properties`, {
+      body: data,
+      method: "POST",
+    });
+  }
