caf16b9d1a0562d0a3417cdeb567cbcf0c58c682
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-10-16 13:17:03 -0500

    ADD: save properties functionalilty

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,39 +1,60 @@
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
+import { createSavedProperty } from "../services/user-properties-services";
 
 const StyledDiv = styled.div`
-    grid-column: 2 / 3;
-    grid-row: 1 / 2;
-    width: 258px;
-    height: 148px;
-    border-radius: 8px;
-    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.2);
-    right: calc(20%);
-    display: grid;
-    place-items: center;
-    margin-left: 64px;
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+  width: 258px;
+  height: 148px;
+  border-radius: 8px;
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.2);
+  right: calc(20%);
+  display: grid;
+  place-items: center;
+  margin-left: 64px;
 `;
 
 const ContactCard = ({ login }) => {
-    return (
-        <StyledDiv>
-            {login ? (
-                <div className="flex flex-column gap-md">
-                    <button>Contact Advertiser</button>
-                    <p> Add to favorites</p>
-                </div>
-            ) : (
-                <div className="flex flex-column gap-md">
-                    <p>Log in or Join to contact the advertiser</p>
-                    <button>Login</button>
-                </div>
-            )}
-        </StyledDiv>
-    );
+  // const [saveData, setSaveData] = useState({
+  //     property_id: "",
+  //   });
+  async function handleSaved() {
+    try {
+      await createSavedProperty({ property_id: 105 });
+    } catch (error) {
+      console.error("Error al manejar favoritos:", error.message);
+    }
+  }
+  //   async function handleSubmitSave(event) {
+  //     event.preventDefault();
+  //     console.log(saveData);
+  //     await createContactedProperty({ property_id: saveData.property_id });
+  //   }
+
+  //   function handleChangeSave(event) {
+  //     const { value } = event.target;
+  //     setSaveData({ ...saveData, property_id: value });
+  //   }
+  return (
+    <StyledDiv>
+      {login ? (
+        <div className="flex flex-column gap-md">
+          <button>Contact Advertiser</button>
+          <p onClick={handleSaved}> Add to favorites</p>
+        </div>
+      ) : (
+        <div className="flex flex-column gap-md">
+          <p>Log in or Join to contact the advertiser</p>
+          <button>Login</button>
+        </div>
+      )}
+    </StyledDiv>
+  );
 };
 
 ContactCard.propTypes = {
-    login: PropTypes.string,
+  login: PropTypes.string,
 };
 
 export default ContactCard;
