d6ab37182afbf26ff8b561fadfc3c733c5301b56
george <jorgelitox15@gmail.com>
2023-10-16 10:55:15 -0500

    Added create user-properties

--- src/services/property-services.js
+++ src/services/property-services.js
@@ -1,5 +1,13 @@
 import apiFetch from "./api-fetch";
 
+export function createProperty(propertyData){
+    return apiFetch("/properties", { body: propertyData });
+  }
+
+export function editProperty(id, propertyData){
+    return apiFetch(`/properties/${id}`, { method: "PATCH", body: propertyData})
+}
+
 export function getProperties() {
     return apiFetch("/properties");
 }
