335cc71bc07954dd0c20fc0cb1fb6e846dc9aa73
Alyho <alyhoiz@gmail.com>
2023-10-16 15:04:59 -0500

    Signup details2

--- src/components/signupPage_components/Signuplanding.jsx
+++ src/components/signupPage_components/Signuplanding.jsx
@@ -16,15 +16,29 @@ const ContentContainer = styled.div`
   transform: transladeY(50%);
 `;
 
-const Text = styled.p`
-  text-align: center;
+const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
 `;
 
-const ProfileContainer = styled.p`
+const ProfileContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 54px;
+`;
+
+const ProfileCard = styled.div`
+  display: flex;
+  padding: 16px 20px;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.2);
 `;
 
 const Signuplanding = () => {
@@ -36,16 +50,16 @@ const Signuplanding = () => {
         </Text>
         <Text className="headline2">¿Qué estás buscando?</Text>
         <ProfileContainer>
-          <div>
-            <div className="j-center a-center">
-              <img src="src/assets/rafiki.png" alt="rafiki" />
-            </div>
-          </div>
-          <div>
-            <div className="j-center">
-              <img src="src/assets/pana.png" alt="pana" />
-            </div>
-          </div>
+          <ProfileCard className="j-center Headline6">
+            <img src="src/assets/rafiki.png" alt="rafiki" />
+            <Text>Landlord</Text>
+            <Text className="Subtitle2">You want to rent or sell a home</Text>
+          </ProfileCard>
+          <ProfileCard className="j-center Headline6">
+            <img src="src/assets/pana.png" alt="pana" />
+            <Text>Home seeker</Text>
+            <Text className="Subtitle2">You want to find a home</Text>
+          </ProfileCard>
         </ProfileContainer>
       </ContentContainer>
     </Mainbackground>
