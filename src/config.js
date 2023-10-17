8fe5b251b06f542b6c4c640dacb25c4a71b511fc
Dante Vasquez <jdantevc@gmail.com>
2023-10-17 10:48:09 -0500

    FIX: BASE_URI (render)

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const tokenKey = "codeable-capstone-token";
-//export const BASE_URI = "https://get-that-home-web-service.onrender.com";
-export const BASE_URI = "http://localhost:3000";
+export const BASE_URI = "https://get-that-home-web-service.onrender.com";
+//export const BASE_URI = "http://localhost:3000";
