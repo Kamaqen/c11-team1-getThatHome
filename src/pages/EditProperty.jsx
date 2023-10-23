de63deca075bc45d73f1e22e4b11cef5cb015f92
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-10-23 02:42:59 -0500

    FIX: setformdata problem with params

--- src/pages/EditProperty.jsx
+++ src/pages/EditProperty.jsx
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import styled from '@emotion/styled';
-import Radiobox from '../components/Radiobox';
-import { useNavigate, useParams } from 'react-router-dom';
-import { showProperty, updateProperty } from '../services/property-services';
-import Section from '../components/Section';
-import { InputPropertyFormFacilities, InputPropertyFormTextBox } from '../components/InputPropertyForm';
-import CloudinaryUpload from '../components/createProperty_components/CloudinaryUpload';
-import Button from '../components/Button';
-import { ButtonContainer, InputPropertyFormContainer, RadioBoxGroup, SignUpForm, SquareRadioInput, StyledDiv, StyledLabel } from '../components/createProperty_components/CreatePropertyStyles';
-import Input from '../components/Inputs';
-import { RiMoneyDollarCircleLine } from "react-icons/ri"
+import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import Radiobox from "../components/Radiobox";
+import { useNavigate, useParams } from "react-router-dom";
+import { showProperty, updateProperty } from "../services/property-services";
+import Section from "../components/Section";
+import {
+  InputPropertyFormFacilities,
+  InputPropertyFormTextBox,
+} from "../components/InputPropertyForm";
+import CloudinaryUpload from "../components/createProperty_components/CloudinaryUpload";
+import Button from "../components/Button";
+import {
+  ButtonContainer,
+  InputPropertyFormContainer,
+  RadioBoxGroup,
+  SignUpForm,
+  SquareRadioInput,
+  StyledDiv,
+  StyledLabel,
+} from "../components/createProperty_components/CreatePropertyStyles";
+import Input from "../components/Inputs";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
-const MoneySymbol = <RiMoneyDollarCircleLine/>
+const MoneySymbol = <RiMoneyDollarCircleLine />;
 
 const Rectangle = styled.div`
   width: 1px;
   align-self: stretch;
-  background: var(--LightGray, #8E8E8E);
-`
+  background: var(--LightGray, #8e8e8e);
+`;
 
 const EditForm = () => {
   const { id } = useParams();
@@ -26,21 +37,27 @@ const EditForm = () => {
   const userID = sessionStorage.getItem("userId");
   const [propertyData, setPropertyData] = useState({});
   const [formData, setFormData] = useState({
-    rent_value: '',
-    property_type: '',
+    rent_value: 0,
+    bedrooms: 0,
+    bathrooms: 0,
+    property_type: "",
     operation_type: "rent",
-    urls: "",
-    description: '',
+    urls: [],
+    description: "",
+    address: "",
     pet_friendly: false,
-    area: '',
-    property_price: '',
-    maintenance_price: '',
+    area: "",
+    property_price: 0,
+    maintenance_price: 0,
     is_active: true,
+    longitude: "",
+    latitude: "",
+    user_id: userID,
   });
   const [imageUrls, setImageUrls] = React.useState([]);
   const [operationType, setOperationType] = React.useState("rent");
 
-    const handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
 
     setFormData((prevFormData) => {
@@ -66,7 +83,7 @@ const EditForm = () => {
         [name2]: "",
       };
     });
-  }
+  };
 
   const handleRent = () => {
     handleChangeOperation("property_price");
@@ -79,11 +96,12 @@ const EditForm = () => {
     setOperationType("sale");
     formData.operation_type = "sale";
   };
-  
+
   async function handleShowProperty() {
     try {
-    const property = await showProperty(propertyId);
-    setPropertyData(property);
+      const property = await showProperty(propertyId);
+      console.log();
+      setPropertyData(property);
     } catch (error) {
       console.error("Error fetching property:", error);
     }
@@ -91,41 +109,51 @@ const EditForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (imageUrls.length !== 0){
-      formData.urls = imageUrls
+    if (imageUrls.length !== 0) {
+      formData.urls = imageUrls;
     }
     console.log(formData);
     updateProperty(propertyId, formData)
-    // Esta parte funciona (llenar la nueva data en el localStorage)
-      .then(response => {
-        const propertiesArray = JSON.parse(localStorage.getItem("propertiesData"));
-        const propertyIndex = propertiesArray.findIndex(property => Number.parseInt(property.id) === Number.parseInt(propertyId));
+      // Esta parte funciona (llenar la nueva data en el localStorage)
+      .then((response) => {
+        const propertiesArray = JSON.parse(
+          localStorage.getItem("propertiesData")
+        );
+        const propertyIndex = propertiesArray.findIndex(
+          (property) =>
+            Number.parseInt(property.id) === Number.parseInt(propertyId)
+        );
         if (propertyIndex !== -1) {
           propertiesArray[propertyIndex] = {
             ...propertiesArray[propertyIndex],
             ...formData,
-        };
-        console.log(propertiesArray);
-        localStorage.clear();
-        localStorage.setItem("propertiesData", JSON.stringify(propertiesArray));
-      } else {
-        console.error(`Property with id ${propertyId} not found.`);
-      }
+          };
+          console.log(propertiesArray);
+          localStorage.clear();
+          localStorage.setItem(
+            "propertiesData",
+            JSON.stringify(propertiesArray)
+          );
+        } else {
+          console.error(`Property with id ${propertyId} not found.`);
+        }
         console.log("Property updated successfully:", response);
         navigate("/my_properties");
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error updating property:", error);
       });
   };
-
 
   useEffect(() => {
     handleShowProperty();
   }, []);
 
   useEffect(() => {
-    setFormData({
+    setFormData(() => ({
+      bedrooms: propertyData.bedrooms,
+      bathrooms: propertyData.bathrooms,
+      address: propertyData.address,
       rent_value: propertyData.rent_value,
       property_type: propertyData.property_type,
       operation_type: propertyData.operation_type,
@@ -136,149 +164,192 @@ const EditForm = () => {
       property_price: propertyData.property_price,
       maintenance_price: propertyData.maintenance_price,
       is_active: propertyData.is_active,
-    });
+      user_id: propertyData.user_id,
+    }));
   }, [propertyData]);
-
 
   return (
     <>
-    <Section align="flex-start">
-    <InputPropertyFormContainer>
-    <div className="headline4">Edit your property listing</div>
-    {formData.area === "" ? (
+      <Section align="flex-start">
+        <InputPropertyFormContainer>
+          <div className="headline4">Edit your property listing</div>
+          {formData.area === "" ? (
             // Display a loading indicator while data is being fetched
             <div>Loading...</div>
-    ) : (
-    <SignUpForm onSubmit={handleSubmit}>
-    <RadioBoxGroup>
-    <div className='overline'>Operation Type</div>
-    <ButtonContainer>
-    <Radiobox
-      style={{ padding: "8px" }}
-      variant={operationType === "rent" ? "Active" : "Inactive"}
-      onClick={handleRent}
-    >
-      Rent
-    </Radiobox>
-    <Rectangle />
-    <Radiobox
-      style={{ padding: "8px", borderRadius: "0px 8px 8px 0px" }}
-      variant={operationType === "sale" ? "Active" : "Inactive"}
-      onClick={handleSale}
-    >
-      Sale
-    </Radiobox>
-    </ButtonContainer>
-    </RadioBoxGroup>
-      { operationType === "rent" ?
-      <>
-      <Input
-        label="Monthly rent"
-        name="rent_value"
-        type= "number"
-        placeholder="2000"
-        value={formData.rent_value}
-        onChange={handleChange}
-        width={"356px"}
-        icon1={MoneySymbol}
-      />
-      <Input
-        label="Maintenance"
-        name="maintenance_price"
-        type = "number"
-        placeholder="100"
-        value={formData.maintenance_price}
-        onChange={handleChange}
-        icon1={MoneySymbol}
-        width={"356px"}
-      />
-      </>
-      : 
-      <Input
-        label="Price"
-        name="property_price"
-        type="number"
-        placeholder="2000"
-        value={formData.property_price}
-        onChange={handleChange}
-        width={"356px"}
-        icon1={MoneySymbol}
-      />
-      } 
-      <div style={{display:"flex", flexDirection:"column", alignItems:"flex-start", gap:"4px"}}>
-      <StyledLabel>Property type</StyledLabel>
-      <StyledDiv>
-      <div>
-      <SquareRadioInput
-        type="radio"
-        name="property_type"
-        value="apartment"
-        checked={formData.property_type === "apartment"}
-        onChange={handleChange}
-      />
-      <label htmlFor="property_type_apartment">Apartment</label>
-      </div>
-      <div>
-      <SquareRadioInput
-        type="radio"
-        name="property_type"
-        value="house"
-        checked={formData.property_type === "house"}
-        onChange={handleChange}
-      />
-      <label htmlFor="property_type_house">House</label>
-      </div>
-      </StyledDiv>
-      </div>
-      <div className="flex flex-row">
-        <InputPropertyFormFacilities
-          label="Area in m2"
-          name="area"
-          type="number"
-          placeholder="##"
-          value={formData.area}
-          onChange={handleChange}
-        />
-      </div>
-      { operationType === "rent" ?
-      <>
-      <div style={{display:"flex", flexDirection:"column", alignItems:"flex-start", gap:"4px"}}>
-      <StyledLabel>Pet Policy</StyledLabel>
-      <div style={{display:'flex', flexDirection:'row', alignItems: 'center'}}>
-      <SquareRadioInput
-        name="pet_friendly"
-        type="checkbox"
-        checked={formData.pet_friendly}
-        onChange={handleChange}
-      />
-      <label htmlFor="pet_friendly">Pets allowed</label>
-      </div>
-      <p style={{fontSize:"12px"}}>Allowing pets increases the likehood of renters liking the property by 9001%. <br/>
-      It also makes you a better person</p>
-      </div>
-      </>
-      : null}
-      <InputPropertyFormTextBox
-        label="About this property"
-        name="description"
-        type="text"
-        placeholder="My apartment is great because..."
-        value={formData.description}
-        onChange={handleChange}
-      />
-      <p style={{fontSize:"12px"}}>Renter will read this first, so highlight any features or important information the apartment has.</p>
-      <div>
-      <p style={{fontSize:"20px", fontWeight: "600", marginBottom:"15px"}}>Photos</p>
-      <p style={{fontSize:"12px", textTransform:"uppercase", fontWeight: "400", marginBottom:"15px"}}>Upload as many photos as you wish</p>
-      <div>
-        <CloudinaryUpload setImageUrls={setImageUrls}/>
-      </div>
-      </div>
-      <Button type="submit">Publish Property Listing</Button>
-      </SignUpForm>
-    )}
-      </InputPropertyFormContainer>
-    </Section>
+          ) : (
+            <SignUpForm onSubmit={handleSubmit}>
+              <RadioBoxGroup>
+                <div className="overline">Operation Type</div>
+                <ButtonContainer>
+                  <Radiobox
+                    style={{ padding: "8px" }}
+                    variant={operationType === "rent" ? "Active" : "Inactive"}
+                    onClick={handleRent}
+                  >
+                    Rent
+                  </Radiobox>
+                  <Rectangle />
+                  <Radiobox
+                    style={{ padding: "8px", borderRadius: "0px 8px 8px 0px" }}
+                    variant={operationType === "sale" ? "Active" : "Inactive"}
+                    onClick={handleSale}
+                  >
+                    Sale
+                  </Radiobox>
+                </ButtonContainer>
+              </RadioBoxGroup>
+              {operationType === "rent" ? (
+                <>
+                  <Input
+                    label="Monthly rent"
+                    name="rent_value"
+                    type="number"
+                    placeholder="2000"
+                    value={formData.rent_value}
+                    onChange={handleChange}
+                    width={"356px"}
+                    icon1={MoneySymbol}
+                  />
+                  <Input
+                    label="Maintenance"
+                    name="maintenance_price"
+                    type="number"
+                    placeholder="100"
+                    value={formData.maintenance_price}
+                    onChange={handleChange}
+                    icon1={MoneySymbol}
+                    width={"356px"}
+                  />
+                </>
+              ) : (
+                <Input
+                  label="Price"
+                  name="property_price"
+                  type="number"
+                  placeholder="2000"
+                  value={formData.property_price}
+                  onChange={handleChange}
+                  width={"356px"}
+                  icon1={MoneySymbol}
+                />
+              )}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  gap: "4px",
+                }}
+              >
+                <StyledLabel>Property type</StyledLabel>
+                <StyledDiv>
+                  <div>
+                    <SquareRadioInput
+                      type="radio"
+                      name="property_type"
+                      value="apartment"
+                      checked={formData.property_type === "apartment"}
+                      onChange={handleChange}
+                    />
+                    <label htmlFor="property_type_apartment">Apartment</label>
+                  </div>
+                  <div>
+                    <SquareRadioInput
+                      type="radio"
+                      name="property_type"
+                      value="house"
+                      checked={formData.property_type === "house"}
+                      onChange={handleChange}
+                    />
+                    <label htmlFor="property_type_house">House</label>
+                  </div>
+                </StyledDiv>
+              </div>
+              <div className="flex flex-row">
+                <InputPropertyFormFacilities
+                  label="Area in m2"
+                  name="area"
+                  type="number"
+                  placeholder="##"
+                  value={formData.area}
+                  onChange={handleChange}
+                />
+              </div>
+              {operationType === "rent" ? (
+                <>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-start",
+                      gap: "4px",
+                    }}
+                  >
+                    <StyledLabel>Pet Policy</StyledLabel>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <SquareRadioInput
+                        name="pet_friendly"
+                        type="checkbox"
+                        checked={formData.pet_friendly}
+                        onChange={handleChange}
+                      />
+                      <label htmlFor="pet_friendly">Pets allowed</label>
+                    </div>
+                    <p style={{ fontSize: "12px" }}>
+                      Allowing pets increases the likehood of renters liking the
+                      property by 9001%. <br />
+                      It also makes you a better person
+                    </p>
+                  </div>
+                </>
+              ) : null}
+              <InputPropertyFormTextBox
+                label="About this property"
+                name="description"
+                type="text"
+                placeholder="My apartment is great because..."
+                value={formData.description}
+                onChange={handleChange}
+              />
+              <p style={{ fontSize: "12px" }}>
+                Renter will read this first, so highlight any features or
+                important information the apartment has.
+              </p>
+              <div>
+                <p
+                  style={{
+                    fontSize: "20px",
+                    fontWeight: "600",
+                    marginBottom: "15px",
+                  }}
+                >
+                  Photos
+                </p>
+                <p
+                  style={{
+                    fontSize: "12px",
+                    textTransform: "uppercase",
+                    fontWeight: "400",
+                    marginBottom: "15px",
+                  }}
+                >
+                  Upload as many photos as you wish
+                </p>
+                <div>
+                  <CloudinaryUpload setImageUrls={setImageUrls} />
+                </div>
+              </div>
+              <Button type="submit">Publish Property Listing</Button>
+            </SignUpForm>
+          )}
+        </InputPropertyFormContainer>
+      </Section>
     </>
   );
 };
