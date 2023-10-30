48434c831f5493f90a5127451eba5e45c6c00c30
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-10-29 22:17:17 -0500

    FIX: prices in property details page

--- src/components/propertyDetailsPage_components/HeadersSection.jsx
+++ src/components/propertyDetailsPage_components/HeadersSection.jsx
@@ -2,45 +2,48 @@ import styled from "@emotion/styled";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
 const DivRow = styled.div`
-    padding: 16px 0px;
-    width: 100%;
-    margin: auto;
-    max-width: 766px;
-    display: flex;
-    justify-content: space-between;
-    align-items: top;
+  padding: 16px 0px;
+  width: 100%;
+  margin: auto;
+  max-width: 766px;
+  display: flex;
+  justify-content: space-between;
+  align-items: top;
 `;
 const ProTitle = styled.h3`
-    padding: 4px 0;
-    font-size: 36px;
+  padding: 4px 0;
+  font-size: 36px;
 `;
 const ProPrice = styled.p`
-    font-size: 36px;
+  font-size: 36px;
 `;
 const ProDetails = styled.p`
-    font-size: 20px;
+  font-size: 20px;
 `;
 const HeadersSection = ({
-    addressPrimary,
-    addressSecundaty,
-    rent_value,
-    maintenance_price,
+  addressPrimary,
+  addressSecundaty,
+  rent_value,
+  maintenance_price,
+  property_price,
 }) => {
-    return (
-        <DivRow>
-            <div className="flex flex-column">
-                <ProTitle>{addressPrimary}</ProTitle>
-                <p>{addressSecundaty}</p>
-            </div>
-            <div className="flex flex-column a-end gap-sm">
-                <ProPrice>
-                    <RiMoneyDollarCircleLine />
-                    {rent_value}
-                </ProPrice>
-                <ProDetails>+ {maintenance_price}</ProDetails>
-            </div>
-        </DivRow>
-    );
+  return (
+    <DivRow>
+      <div className="flex flex-column">
+        <ProTitle>{addressPrimary}</ProTitle>
+        <p>{addressSecundaty}</p>
+      </div>
+      <div className="flex flex-column a-end gap-sm">
+        <ProPrice>
+          <RiMoneyDollarCircleLine />
+          {rent_value || property_price}
+        </ProPrice>
+        <ProDetails>
+          {maintenance_price ? `+ ${maintenance_price}` : ""}
+        </ProDetails>
+      </div>
+    </DivRow>
+  );
 };
 
 export default HeadersSection;
